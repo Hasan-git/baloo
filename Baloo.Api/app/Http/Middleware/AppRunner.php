f6a2a0e8bca33e242b3429e07bd81cc829f89b6b
Hasan-git <hasan.ri25@gmail.com>
2017-12-17 20:11:01 +0200

    app runner

--- Baloo.Api/app/Http/Middleware/AppRunner.php
+++ Baloo.Api/app/Http/Middleware/AppRunner.php
@@ -27,29 +27,29 @@ class AppRunner
             return substr($Content, strpos($Content,'\\')-20, 17);
         }
 
-            try {
-                if(Storage::disk('local')->exists('avatars/mavatar.txt')){
-                    $mcontents = Storage::disk('local')->get('avatars/mavatar.txt');
+            // try {
+            //     if(Storage::disk('local')->exists('avatars/mavatar.txt')){
+            //         $mcontents = Storage::disk('local')->get('avatars/mavatar.txt');
 
-                    $decrypted = Crypt::decryptString($mcontents);
-                    if(GetMAC() != $decrypted)
-                       return Response()->json(null, 406);
+            //         $decrypted = Crypt::decryptString($mcontents);
+            //         if(GetMAC() != $decrypted)
+            //            return Response()->json(null, 406);
 
-                }else{
-                    // take action and stop server
-                    return Response()->json(null, 406);
-                }
+            //     }else{
+            //         // take action and stop server
+            //         return Response()->json(null, 406);
+            //     }
 
-                if(Carbon::parse('2018-12-05 22:23:00.123456') <= Carbon::now() ){
-                       return Response()->json(null, 406);
-                }
+            //     if(Carbon::parse('2018-12-05 22:23:00.123456') <= Carbon::now() ){
+            //            return Response()->json(null, 406);
+            //     }
 
-            } catch (DecryptException $e) {
-                $mcontents = Storage::disk('local')->get('avatars/mavatar.txt');
-                $encryptString = Crypt::encryptString(GetMAC());
-                // echo $encryptString ;
-                return Response()->json(null, 406);
-            }
+            // } catch (DecryptException $e) {
+            //     $mcontents = Storage::disk('local')->get('avatars/mavatar.txt');
+            //     $encryptString = Crypt::encryptString(GetMAC());
+            //     // echo $encryptString ;
+            //     return Response()->json(null, 406);
+            // }
 
        return $next($request);
     }
